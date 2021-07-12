540bb20c93dccaa78b56835c7e0fed912ab5727f
Gilberto <juniorfredes8@gmail.com>
2021-07-12 08:28:04 -0300

    codigo compilado

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+#define MAX 20
+
 int main(int argc, char *argv[]) {
+	
+	int vetor[MAX] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+	
+	
 	
 	
 	return 0;
