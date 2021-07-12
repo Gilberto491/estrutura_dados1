34795f00088ca0d6fb547c3edbb4e9c8186895f6
Gilberto <juniorfredes8@gmail.com>
2021-07-12 08:48:28 -0300

    add opções

--- main.c
+++ main.c
@@ -8,8 +8,17 @@
 int main(int argc, char *argv[]) {
 	
 	int vetor[MAX] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+	int opcao = 0;
 	
-	
+	while(opcao != 5) {
+		printf("Escolha uma opção:\n");
+		printf("1 - Preencher um valor de codigo numerico em 1 posicao (de 1 a 20)\n");
+		printf("2 - Consultar o valor de determinada posicao\n");
+		printf("3 - Excluir um valor registrado anteriormente\n");
+		printf("4 - Consultar o numero de posicoes ocupadas\n");
+		printf("5 - Sair do programa");
+		scanf("%d", &opcao);
+	}
 	
 	
 	return 0;
