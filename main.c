39b12c2301d9b59536350fb05287932d9caaf0c4
Gilberto <juniorfredes8@gmail.com>
2021-07-12 09:44:55 -0300

    fix(corrigir vetor)

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[]) {
 	
-	int vetor[MAX] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+	int vetor[MAX] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 	char opcao[1];
 	
 	while(strcmp (opcao,"e") != 0) {
